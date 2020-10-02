d3dc1c2171f82b605104bcbaa5174a8b5eec8be1
mistahpig <dspark410@gmail.com>
2020-10-02 14:00:36 -0700

    Updates

---
+++ precache-manifest.3f93265568b45a73b88bde0870362910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cd4f9b9c55c2d6484ee8295a54f8fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "efc698d0581814a063a6",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4636271649cef355944b",
+    "url": "./static/css/main.a3a55214.chunk.css"
+  },
+  {
+    "revision": "efc698d0581814a063a6",
+    "url": "./static/js/2.8512cd5d.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "./static/js/2.8512cd5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4636271649cef355944b",
+    "url": "./static/js/main.db37d6c6.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.672615eb5a7b329e78ca4314204206b0.js"
+  "./precache-manifest.3f93265568b45a73b88bde0870362910.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
