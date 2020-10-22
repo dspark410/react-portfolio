d1ea8a30b8d0ac111f69244c42b770bf97d57cb9
mistahpig <dspark410@gmail.com>
2020-10-21 17:28:54 -0700

    adds email icon/link

--- src/App.js
+++ src/App.js
@@ -1,60 +1,114 @@
-import React from 'react';
-import './App.css';
-import Footer from "./components/Footer"
-import ProjectBtn from './components/ProjectBtn';
-import { FaGithub } from "react-icons/fa"
-import { FaLinkedin } from "react-icons/fa"
-import { ImProfile } from "react-icons/im";
+import React from 'react'
+import './App.css'
+import Footer from './components/Footer'
+import ProjectBtn from './components/ProjectBtn'
+import { FaGithub } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+import { ImProfile } from 'react-icons/im'
+import { HiOutlineMail } from 'react-icons/hi'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="container">
-
-      <div className="main">
-        <div className="box">
-        <img className="profile-pic" width="200px" height="200px" src={process.env.PUBLIC_URL + "/images/profilepic.jpg"} alt="David Park" />
-        <div className="border2">
-          
+    <div className='container'>
+      <div className='main'>
+        <div className='box'>
+          <img
+            className='profile-pic'
+            width='200px'
+            height='200px'
+            src={process.env.PUBLIC_URL + '/images/profilepic.jpg'}
+            alt='David Park'
+          />
+          <div className='border2'></div>
         </div>
-        </div>
-        
-        <h1 className="name">David S. Park</h1>
 
-        <h2 className="title" style={{ textTransform: "uppercase", color: "cornflowerblue", fontWeight: "600", fontSize: "1.0rem" }}>Full Stack Web Developer</h2>
-        <div className="mainsummary-container">
-          <div className="summary-container">
-            <p style={{color:"black", fontWeight:"bold"} }className="summary">Recent bootcamp graduate from the UC Berkeley Extension Full Stack Web Development Program</p>
+        <h1 className='name'>David S. Park</h1>
+
+        <h2
+          className='title'
+          style={{
+            textTransform: 'uppercase',
+            color: 'cornflowerblue',
+            fontWeight: '600',
+            fontSize: '1.0rem',
+          }}
+        >
+          Full Stack Web Developer
+        </h2>
+        <div className='mainsummary-container'>
+          <div className='summary-container'>
+            <p
+              style={{ color: 'black', fontWeight: 'bold' }}
+              className='summary'
+            >
+              Recent bootcamp graduate from the UC Berkeley Extension Full Stack
+              Web Development Program
+            </p>
           </div>
-          <div className="summary-container">
-            <p style={{color:"black", fontWeight:"bold"}} className="summary">During my free time I love to play video games, cook, and eat lots of food.</p>
+          <div className='summary-container'>
+            <p
+              style={{ color: 'black', fontWeight: 'bold' }}
+              className='summary'
+            >
+              During my free time I love to play video games, cook, and eat lots
+              of food.
+            </p>
           </div>
-          <div className="summary-container">
-            <p style={{color:"black", fontWeight:"bold"}} className="summary">I'm also a personally certified Food Adventurer & Eater.</p>
+          <div className='summary-container'>
+            <p
+              style={{ color: 'black', fontWeight: 'bold' }}
+              className='summary'
+            >
+              I'm also a personally certified Food Adventurer & Eater.
+            </p>
           </div>
         </div>
 
-        <div className="icon-row">
-          <a className="iconleft" href= {process.env.PUBLIC_URL + "/assets/DPark_Resume.pdf"} rel="noopener noreferrer" download>
-            <ImProfile className="iconborder" />
+        <div className='icon-row'>
+          <a
+            className='iconleft'
+            href='mailto:dspark410@gmail.com'
+            rel='noopener noreferrer'
+          >
+            <HiOutlineMail
+              className='iconborder'
+              style={{ fontSize: '16px' }}
+            />
           </a>
-          <a className="iconright" href="https://github.com/dspark410" target="_blank" rel="noopener noreferrer">
-            <FaGithub className="iconborder" />
+          <a
+            className='iconleft'
+            href={process.env.PUBLIC_URL + '/assets/DPark_Resume.pdf'}
+            rel='noopener noreferrer'
+            download
+          >
+            <ImProfile className='iconborder' />
           </a>
-          <a className="iconright" href="https://www.linkedin.com/in/david-s-park/" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin className="iconborder" />
+          <a
+            className='iconright'
+            href='https://github.com/dspark410'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaGithub className='iconborder' />
           </a>
-
+          <a
+            className='iconright'
+            href='https://www.linkedin.com/in/david-s-park/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaLinkedin className='iconborder' />
+          </a>
         </div>
-        <h1 className="projects"><ProjectBtn /></h1>
+        <h1 className='projects'>
+          <ProjectBtn />
+        </h1>
       </div>
       <Footer />
     </div>
-
-
-
-  );
+  )
 }
 
-export default App;
+export default App
