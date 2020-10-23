228216a6d44718fb65c2bcec392bd5520a87a2c9
mistahpig <dspark410@gmail.com>
2020-10-23 13:16:36 -0700

    adds icon styling

--- src/App.js
+++ src/App.js
@@ -69,40 +69,42 @@ function App() {
         </div>
 
         <div className='icon-row'>
-          <a
-            className='iconleft'
-            href='mailto:dspark410@gmail.com'
-            rel='noopener noreferrer'
-          >
-            <HiOutlineMail
-              className='iconborder'
-              style={{ fontSize: '16px' }}
-            />
-          </a>
-          <a
-            className='iconleft'
-            href={process.env.PUBLIC_URL + '/assets/DPark_Resume.pdf'}
-            rel='noopener noreferrer'
-            download
-          >
-            <ImProfile className='iconborder' />
-          </a>
-          <a
-            className='iconright'
-            href='https://github.com/dspark410'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub className='iconborder' />
-          </a>
-          <a
-            className='iconright'
-            href='https://www.linkedin.com/in/david-s-park/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaLinkedin className='iconborder' />
-          </a>
+          <div className='icon-container'>
+            <a
+              className='icon'
+              href='mailto:dspark410@gmail.com'
+              rel='noopener noreferrer'
+            >
+              <HiOutlineMail
+                className='iconborder'
+                style={{ fontSize: '16px' }}
+              />
+            </a>
+            <a
+              className='icon'
+              href={process.env.PUBLIC_URL + '/assets/DPark_Resume.pdf'}
+              rel='noopener noreferrer'
+              download
+            >
+              <ImProfile className='iconborder' />
+            </a>
+            <a
+              className='icon'
+              href='https://github.com/dspark410'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaGithub className='iconborder' />
+            </a>
+            <a
+              className='icon'
+              href='https://www.linkedin.com/in/david-s-park/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaLinkedin className='iconborder' />
+            </a>
+          </div>
         </div>
         <Skills />
         <h1 className='projects'>
