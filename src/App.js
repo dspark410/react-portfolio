6c1a845d70a759010afb7664a751cd620f51c40e
mistahpig <dspark410@gmail.com>
2020-12-09 12:09:27 -0800

    adds updated projects

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false)
-    }, 3900)
+    }, 3000)
   }, [])
 
   return (
--- src/components/ProjectBtn/index.js
+++ src/components/ProjectBtn/index.js
@@ -22,13 +22,23 @@ const customStyles = {
 Modal.setAppElement('#root')
 
 export default function ProjectBtn() {
+  const [modalProjectIsOpen, setModalProjectIsOpen] = useState(false)
   const [modalIsOpen, setIsOpen] = useState(false)
+  const [modalIsOpen1, setIsOpen1] = useState(false)
   const [modal2IsOpen, setIsOpen2] = useState(false)
   const [modal3IsOpen, setIsOpen3] = useState(false)
   const [modal4IsOpen, setIsOpen4] = useState(false)
 
+  function openProjectModal() {
+    setModalProjectIsOpen(true)
+  }
+
   function openModal() {
     setIsOpen(true)
+  }
+
+  function openModal1() {
+    setIsOpen1(true)
   }
 
   function openModal2() {
@@ -38,12 +48,21 @@ export default function ProjectBtn() {
   function openModal3() {
     setIsOpen3(true)
   }
+
   function openModal4() {
     setIsOpen4(true)
   }
 
+  function closeProjectModal() {
+    setModalProjectIsOpen(false)
+  }
+
   function closeModal() {
     setIsOpen(false)
+  }
+
+  function closeModal1() {
+    setIsOpen1(false)
   }
 
   function closeModal2() {
@@ -59,7 +78,7 @@ export default function ProjectBtn() {
   }
 
   useEffect(() => {
-    Aos.init({ duration: 1500 })
+    Aos.init({ duration: 1000 })
   }, [])
   return (
     <>
@@ -67,6 +86,7 @@ export default function ProjectBtn() {
         <span className='projects'>Projects</span>
       </div>
       <Row>
+        {/* League App ///////////////////////////////////////////////////////////// */}
         <Col
           xs={12}
           sm={12}
@@ -80,8 +100,8 @@ export default function ProjectBtn() {
               <img
                 data-aos='fade-up'
                 className='project-image mb-2'
-                src={process.env.PUBLIC_URL + '/images/covidtracker.png'}
-                alt='Covid-19 Tracker'
+                src={process.env.PUBLIC_URL + '/images/league-stats.png'}
+                alt='League Stats'
               />
             </button>
 
@@ -91,9 +111,107 @@ export default function ProjectBtn() {
               style={customStyles}
             >
               <div className='modal-container'>
+                <h3 className='modal-header'>League Stats</h3>
+                <img
+                  className='modal-image img-fluid'
+                  src={process.env.PUBLIC_URL + '/images/league-stats.png'}
+                  alt='League Stats'
+                />
+                <p className='font-weight-bold w-75 description'>
+                  Inspired by my love for gaming I decided to make an app using
+                  the Riot API for League of Legends. This app displays various
+                  user information from ranked info, mastery info, and details
+                  about match history.
+                </p>
+                <p className='font-weight-bold'>
+                  Technologies Used:
+                  <span className='font-weight-normal'>
+                    {' '}
+                    React, Express.js, Node.js
+                  </span>
+                </p>
+                <p className='font-weight-bolder'>
+                  Note:{' '}
+                  <span className='font-weight-normal'>
+                    Currently, there is no deployment link. This project is
+                    still in progress.
+                  </span>
+                </p>
+                <div className='button-container'>
+                  {/* <a href='' rel='noopener noreferrer' target='_blank'>
+                    <button
+                      disabled='true'
+                      className='footer-button'
+                      style={{
+                        fontFamily: 'Jura, sans-serif',
+                      }}
+                    >
+                      View Site
+                    </button>
+                  </a> */}
+                  <a
+                    href='https://github.com/dspark410/league-of-legends-app'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                  >
+                    <button
+                      className='footer-button'
+                      style={{
+                        fontFamily: 'Jura, sans-serif',
+                      }}
+                    >
+                      GitHub
+                    </button>
+                  </a>
+                </div>
+                <div className='modal-footer'>
+                  <button
+                    className='modal-footer-button'
+                    style={{
+                      fontFamily: 'Jura, sans-serif',
+                    }}
+                    onClick={closeModal}
+                  >
+                    Close
+                  </button>
+                </div>
+                <button className='close-button' onClick={closeModal}>
+                  <CgClose />
+                </button>
+              </div>
+            </Modal>
+          </div>
+        </Col>
+
+        {/* Covid Tracker/////////////////////////////////////////////////////////// */}
+
+        <Col
+          xs={12}
+          sm={12}
+          md={6}
+          lg={4}
+          xl={3}
+          className='justify-content-center'
+        >
+          <div className='image-container'>
+            <button onClick={openModal1} className='image-button'>
+              <img
+                data-aos='fade-up'
+                className='project-image mb-2'
+                src={process.env.PUBLIC_URL + '/images/covidtracker.png'}
+                alt='Covid-19 Tracker'
+              />
+            </button>
+
+            <Modal
+              isOpen={modalIsOpen1}
+              onRequestClose={closeModal1}
+              style={customStyles}
+            >
+              <div className='modal-container'>
                 <h3 className='modal-header'>Covid-19 Tracker</h3>
                 <img
-                  className='modal-image m-auto'
+                  className='modal-image img-fluid'
                   src={process.env.PUBLIC_URL + '/images/covidtracker.png'}
                   alt='Covid-19 Tracker'
                 />
@@ -106,7 +224,7 @@ export default function ProjectBtn() {
                   Technologies Used:
                   <span className='font-weight-normal'>
                     {' '}
-                    React.js, React-Leaflet, Chart.js, Bootstrap
+                    React, React-Leaflet, Chart.js, Bootstrap
                   </span>
                 </p>
                 <div className='button-container'>
@@ -145,18 +263,112 @@ export default function ProjectBtn() {
                     style={{
                       fontFamily: 'Jura, sans-serif',
                     }}
-                    onClick={closeModal}
+                    onClick={closeModal1}
                   >
                     Close
                   </button>
                 </div>
-                <button className='close-button' onClick={closeModal}>
+                <button className='close-button' onClick={closeModal1}>
                   <CgClose />
                 </button>
               </div>
             </Modal>
           </div>
         </Col>
+
+        {/* React Portfolio /////////////////////////////////////////////////////////////// */}
+
+        <Col
+          xs={12}
+          sm={12}
+          md={6}
+          lg={4}
+          xl={3}
+          className='justify-content-center'
+        >
+          <div className='image-container'>
+            <button onClick={openProjectModal} className='image-button'>
+              <img
+                data-aos='fade-up'
+                className='project-image mb-2'
+                src={process.env.PUBLIC_URL + '/images/react-portfolio.png'}
+                alt='react portfolio'
+              />
+            </button>
+
+            <Modal
+              isOpen={modalProjectIsOpen}
+              onRequestClose={closeProjectModal}
+              style={customStyles}
+            >
+              <div className='modal-container'>
+                <h3 className='modal-header'>React Portfolio</h3>
+                <img
+                  className='modal-image img-fluid'
+                  src={process.env.PUBLIC_URL + '/images/react-portfolio.png'}
+                  alt='react portfolio'
+                />
+                <p className='font-weight-bold w-75 description'>
+                  After graduating from bootcamp, I wanted to display all my
+                  projects with a new portfolio using React so here it is!
+                </p>
+                <p className='font-weight-bold'>
+                  Technologies Used:
+                  <span className='font-weight-normal'>
+                    {' '}
+                    React, Email.js, Material-UI, Bootstrap, AOS
+                  </span>
+                </p>
+                <div className='button-container'>
+                  <a
+                    href='https://david-s-park.com'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                  >
+                    <button
+                      className='footer-button'
+                      style={{
+                        fontFamily: 'Jura, sans-serif',
+                      }}
+                    >
+                      View Site
+                    </button>
+                  </a>
+                  <a
+                    href='https://github.com/dspark410/react-portfolio'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                  >
+                    <button
+                      className='footer-button'
+                      style={{
+                        fontFamily: 'Jura, sans-serif',
+                      }}
+                    >
+                      GitHub
+                    </button>
+                  </a>
+                </div>
+                <div className='modal-footer'>
+                  <button
+                    className='modal-footer-button'
+                    style={{
+                      fontFamily: 'Jura, sans-serif',
+                    }}
+                    onClick={closeProjectModal}
+                  >
+                    Close
+                  </button>
+                </div>
+                <button className='close-button' onClick={closeProjectModal}>
+                  <CgClose />
+                </button>
+              </div>
+            </Modal>
+          </div>
+        </Col>
+
+        {/* Hi Ho //////////////////////////////////////////////////////////// */}
 
         <Col
           xs={12}
@@ -183,34 +395,33 @@ export default function ProjectBtn() {
               <div className='modal-container'>
                 <h3 className='modal-header'>Hi Ho</h3>
                 <img
-                  className='modal-image m-auto'
+                  className='modal-image img-fluid'
                   src={process.env.PUBLIC_URL + '/images/hiho.png'}
                   alt='Hi Ho'
                 />
                 <p className='font-weight-bold w-75 description'>
                   For my final group project during bootcamp, we created a job
                   search application using ZipRecruiter's API to retrieve job
-                  listing info and the Numbeo API to display cost of living info
-                  for the city searched.
+                  listing info and utilized the Numbeo API to display cost of
+                  living data for the city searched.
                 </p>
                 <p className='font-weight-bold'>
                   Technologies Used:
                   <span className='font-weight-normal'>
                     {' '}
-                    MongoDB, Express.js, React.js, Node.js, Material-UI
+                    MongoDB, Express.js, React, Node.js, Material-UI
                   </span>
                 </p>
                 <p className='font-weight-bolder'>
                   Note:{' '}
                   <span className='font-weight-normal'>
-                    {' '}
-                    Due to ZipRecruiter API key access expiration, the job
-                    searching functionality is currently unavailable.
+                    Due to ZipRecruiter API access expiration, the job searching
+                    functionality was edited to use the USAjobs API.
                   </span>
                 </p>
                 <div className='button-container'>
                   <a
-                    href='https://hi-ho-frontend.herokuapp.com/'
+                    href='https://hi-ho-job-search.herokuapp.com/'
                     rel='noopener noreferrer'
                     target='_blank'
                   >
@@ -224,7 +435,7 @@ export default function ProjectBtn() {
                     </button>
                   </a>
                   <a
-                    href='https://github.com/dspark410/Hi-Ho-Frontend'
+                    href='https://github.com/dspark410/hi-ho-frontend'
                     rel='noopener noreferrer'
                     target='_blank'
                   >
@@ -258,6 +469,8 @@ export default function ProjectBtn() {
           </div>
         </Col>
 
+        {/* Smasual /////////////////////////////////////////////////////////// */}
+
         <Col
           xs={12}
           sm={6}
@@ -283,7 +496,7 @@ export default function ProjectBtn() {
               <div className='modal-container'>
                 <h3 className='modal-header'>Smasual</h3>
                 <img
-                  className='modal-image m-auto'
+                  className='modal-image img-fluid'
                   src={process.env.PUBLIC_URL + '/images/smasual.png'}
                   alt='Smasual'
                 />
@@ -291,23 +504,21 @@ export default function ProjectBtn() {
                   For my second group project during bootcamp, we created a
                   simple dating application that allowed the user to find a date
                   by gender orientation, age preference, and location using
-                  zipcode. The user could then contact a potential match via
+                  zipcodes. The user could then contact a potential match via
                   email.
                 </p>
                 <p className='font-weight-bold'>
-                  Technologies Used:
+                  Technologies Used:{' '}
                   <span className='font-weight-normal'>
-                    {' '}
                     MySQL, HTML, CSS, JavaScript, Express.js, Node.js,
-                    Nodemailer.js, Firebase
+                    Nodemailer, Firebase.
                   </span>
                 </p>
                 <p className='font-weight-bolder'>
                   Note:{' '}
                   <span className='font-weight-normal'>
-                    {' '}
-                    Due to a limited database there may be unavailable matches
-                    based on user specified criteria.
+                    Due to a limited database of users there may be unavailable
+                    matches based on user specified criteria.
                   </span>
                 </p>
                 <div className='button-container'>
@@ -360,6 +571,8 @@ export default function ProjectBtn() {
           </div>
         </Col>
 
+        {/* Travel Nurse App /////////////////////////////////////////////////////////////////// */}
+
         <Col
           xs={12}
           sm={6}
@@ -385,7 +598,7 @@ export default function ProjectBtn() {
               <div className='modal-container'>
                 <h3 className='modal-header'>Travel Nurses Job Spot</h3>
                 <img
-                  className='modal-image m-auto'
+                  className='modal-image img-fluid'
                   src={process.env.PUBLIC_URL + '/images/nurse.png'}
                   alt='Travel Nurse App'
                 />
@@ -397,20 +610,19 @@ export default function ProjectBtn() {
                   informed decision about the job.
                 </p>
                 <p className='font-weight-bold'>
-                  Technologies Used:
+                  Technologies Used:{' '}
                   <span className='font-weight-normal'> HTML, CSS, jQuery</span>
                 </p>
                 <p className='font-weight-bolder'>
                   Note:{' '}
                   <span className='font-weight-normal'>
-                    {' '}
-                    Due to CORS, the city selection menu will load very slowly
-                    after the application is loaded.
+                    Unfortunately, due to CORS the city selection menu will load
+                    very slowly after the application is loaded.
                   </span>
                 </p>
                 <div className='button-container'>
                   <a
-                    href='https://dspark410.github.io/Travel-Nurse-App/'
+                    href='https://dspark410.github.io/travel-nurse-app/'
                     rel='noopener noreferrer'
                     target='_blank'
                   >
